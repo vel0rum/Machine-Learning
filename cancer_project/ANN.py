e28f5c2c61be760de8df7d57e01acf1e6357202b
vel0rum <m.altaykan@gmail.com>
2020-11-24 15:24:26 +0000

    started cancer project for work

---
+++ cancer_project/ANN.py
@@ -0,0 +1,138 @@
+# Artificial Neural Network
+
+# Importing the libraries
+import pytest
+import numpy as np
+import pandas as pd
+from google.colab import drive
+
+import tensorflow as tf
+
+# Part 1 - Data Preprocessing
+
+# Importing the dataset
+def import_local_data(file_path):
+    """import data file and return pandas dataframe"""
+    raw_df = pd.read_csv(file_path)
+    return raw_df
+
+local_file_path = 'breast-cancer.csv'
+raw_data = import_local_data(local_file_path)
+
+"""
+Dataset consists of nine features and one binary dependent variable.
+
+age is broken down into six categories.
+
+menopause is broken down into three categories.
+
+tumor-size is broken down into eleven categories. 5-9 and 10-14 were interpreted as dates in the .csv file (05-Sept and Oct-14), so I modified them to what they should be.
+
+inv-nodes is broken down into seven categories. Again, some entries were interpreted as dates and were corrected. There were no categories for people with 18-23 or 27+ inv-nodes.
+
+node-caps is either yes or no, 8 entries had a '?'.
+
+deg-malig is either 1, 2, or 3.
+
+breast is either left or right.
+
+breast-quad is split into five categories with one entry as a '?'. The value of left_low + left_up is much greater than the total number of 'lefts' in the breast column. Potentially erronious data?
+
+irradiat is either yes or no.
+
+The dependent variable is either recurrence-events or no-recurrence-events.
+
+
+"""
+
+
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+print(y)
+
+# Encoding categorical data
+# Label Encoding the "Gender" column
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+print(X)
+# One Hot Encoding the "Geography" column
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Part 2 - Building the ANN
+
+# Initializing the ANN
+ann = tf.keras.models.Sequential()
+
+# Adding the input layer and the first hidden layer
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+
+# Adding the second hidden layer
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+
+# Adding the output layer
+ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+# Part 3 - Training the ANN
+
+# Compiling the ANN
+ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Training the ANN on the Training set
+ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
+
+# Part 4 - Making the predictions and evaluating the model
+
+# Predicting the result of a single observation
+
+"""
+Homework:
+Use our ANN model to predict if the customer with the following informations will leave the bank: 
+Geography: France
+Credit Score: 600
+Gender: Male
+Age: 40 years old
+Tenure: 3 years
+Balance: $ 60000
+Number of Products: 2
+Does this customer have a credit card? Yes
+Is this customer an Active Member: Yes
+Estimated Salary: $ 50000
+So, should we say goodbye to that customer?
+
+Solution:
+"""
+
+print(ann.predict(sc.transform([[1, 0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])) > 0.5)
+
+"""
+Therefore, our ANN model predicts that this customer stays in the bank!
+Important note 1: Notice that the values of the features were all input in a double pair of square brackets. That's because the "predict" method always expects a 2D array as the format of its inputs. And putting our values into a double pair of square brackets makes the input exactly a 2D array.
+Important note 2: Notice also that the "France" country was not input as a string in the last column but as "1, 0, 0" in the first three columns. That's because of course the predict method expects the one-hot-encoded values of the state, and as we see in the first row of the matrix of features X, "France" was encoded as "1, 0, 0". And be careful to include these values in the first three columns, because the dummy variables are always created in the first columns.
+"""
+
+# Predicting the Test set results
+y_pred = ann.predict(X_test)
+y_pred = (y_pred > 0.5)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
